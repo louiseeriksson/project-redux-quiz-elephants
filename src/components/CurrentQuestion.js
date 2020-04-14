0ce825a38fe7aa3da877edd3a3fd5803e26799ab
Mathias Thorström <thisen@gmail.com>
2020-04-14 14:40:41 +0200

    updated button component with conditional rendering on currentqestion.js

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,22 +1,22 @@
+/* eslint-disable react/self-closing-comp */
 import React from 'react'
 import { useSelector } from 'react-redux'
-import {Link} from 'react-router-dom'
-import {Button} from './Button'
+import { Link } from 'react-router-dom'
+import { Button } from './Button'
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuesionIndex])
-
+  const userDone = useSelector((state) => state.quiz.quizOver)
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
 
-
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
-      <Link to={`/resultPage`}>
-      <Button info="ResultPage" />
-      </Link>
+      {userDone ?  <Link to="/resultPage"><Button info="ResultPage" /> 
+      </Link> : <Link to={`/questions/${question.id}`}><Button info="next question" /></Link> }
+    
     </div>
   )
 }
